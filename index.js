e55959c4992ef29411131606d68aa22d9e83b711
Tom Wilson <tom@jackhq.com>
2017-06-06 22:17:07 -0400

    refactored to better implementation of curryN

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 module.exports = curryN
 
-function curryN(n, fn) {
-  let _args = []
-  return function handler(...args) {
-    _args = _args.concat(args)
-    if (not(equals(_args.length, n))) {
-      return handler
-    }
-    const result = fn.apply(null, _args)
-    _args.length = 0
-    return result
+function curryN(arity, fn, ...curried) {
+  return function(...called) {
+    const args = [...curried, ...called]
+    return arity <= args.length ? fn(...args) : curryN(arity, fn, ...args)
   }
-}
-
-function equals(a, b) {
-  return a === b
-}
-
-function not(a) {
-  return !a
 }
